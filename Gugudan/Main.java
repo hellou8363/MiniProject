4f916ad8a8fbc3d018df7cf39fceedb84b4016c3
YellowHan <89592727+YellowHan@users.noreply.github.com>
2021-10-26 17:41:50 +0900

    Main

---
+++ Gugudan/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Output output = new Output();
+        Input input = new Input();
+
+        output.info();
+        output.printMul();
+        input.setUserNum();
+        output.result();
+
+    }
+}
