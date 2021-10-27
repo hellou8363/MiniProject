dff09d52bdd6a7bd8518d7f95566f2a4f3497f71
YellowHan <89592727+YellowHan@users.noreply.github.com>
2021-10-27 12:45:51 +0900

    Add files via upload

--- Gugudan/Main.java
+++ Gugudan/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Output output = new Output();
-        Input input = new Input();
 
-        output.info();
-        output.printMul();
-        input.setUserNum();
-        output.result();
-
+        UpTo100 upTo100 = new UpTo100();
+        upTo100.upTo100();
     }
 }
