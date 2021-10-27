18e813859df065f1ff9e623adffaa33680159e22
YellowHan <89592727+YellowHan@users.noreply.github.com>
2021-10-27 12:47:14 +0900

    정답, 오답 return 추가

--- Gugudan/Output.java
+++ Gugudan/Output.java
@@ -1,6 +1,11 @@
 public class Output {
+    boolean ox;
+
     void info(){
-        System.out.println("===== 구구단 게임 시작 =====");
+        System.out.println("===== 구구단 게임 =====");
+        System.out.println("정답 : +5점 / 오답 : -3점");
+        System.out.println("100점 도달 시 정답 횟수, 오답 횟수 출력");
+        System.out.println("===== 게임 시작 =====");
     }
 
     void printMul(){
@@ -9,14 +14,16 @@ public class Output {
         System.out.print(cal.getNum1()+" * "+ cal.getNum2()+" = ");
     }
 
-    void result(){
+    boolean result(){
         Input input = new Input();
         Calculation calculation = new Calculation();
         if(calculation.cal()==input.getUserNum()){
             System.out.println("정답-!!");
+            return ox = true;
         }
         else {
             System.out.println("틀렸습니다\n정답은 "+calculation.cal());
+            return ox = false;
         }
     }
 }
